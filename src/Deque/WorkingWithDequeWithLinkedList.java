f4ca5e6a01b41ab5b0b2e55ae5af31c82d9087bc
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-18 00:39:05 +0530

    Modification in Deque class

--- src/Deque/WorkingWithDequeWithLinkedList.java
+++ src/Deque/WorkingWithDequeWithLinkedList.java
@@ -3,6 +3,7 @@ package Deque;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class WorkingWithDequeWithLinkedList {
 
@@ -14,10 +15,15 @@ public class WorkingWithDequeWithLinkedList {
         d.offerFirst(5);
         d.offerLast(15);
 
+        d.stream()
+                .forEach(System.out::println);
+
+        System.out.println("----------");
 
         System.out.println(d.peekFirst());
         System.out.println(d.peekLast());
 
+        System.out.println("-----------");
         //Remove from First Position
         d.pollFirst();
 
